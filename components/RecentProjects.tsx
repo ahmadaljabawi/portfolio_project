b9e6f5c799076c4777b3006f66511642d2164a07
ahmadaljabawi <ahmedaljbaoe@gmail.com>
2024-12-16 15:55:43 +0300

    Client section done

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,7 +1,6 @@
 import { projects } from '@/data'
 import React from 'react'
 import { PinContainer } from './ui/3d-pin'
-import { div, p } from 'framer-motion/client'
 import { FaLocationArrow } from 'react-icons/fa6'
 
 const RecentProjects = () => {
